371ebae4e49c62b8d9a7603df0c165ad958f1366
Brian Cheng <brianc2730@gmail.com>
2023-11-28 17:45:05 -0800

    Initial Window + Triangle

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Your OpenGL code here
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+void Initialize(int argc, char** argv) {
+    glutInit(&argc, argv);
+    // glutInitContextVersion(4,3);
+    // glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(300,150);
+    glutCreateWindow("OpenGL Example");
+}
+
+int main(int argc, char** argv) {
+    
+    Initialize(argc, argv);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}

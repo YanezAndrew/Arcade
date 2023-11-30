7aac9528c0a5d8477d0f40d86cf9865dbef8b50a
KauriMai <kaurimai@ucsb.edu>
2023-11-29 16:21:25 -0800

    Setup + Empty Window (Track A)

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#define GL_SILENCE_DEPRECATION
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Your OpenGL code here
+    glFlush();
+}
+
+void Initialize(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(300,150);
+    glutCreateWindow("OpenGL Example");
+}
+
+int main(int argc, char** argv) {
+    Initialize(argc, argv);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
